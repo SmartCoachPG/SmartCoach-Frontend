3c20efbb30db92b343ec33e4f4156773804ca34c
Johanna Bolívar <bolivarjo@javeriana.edu.co>
2023-10-07 23:15:15 -0500

    Conexión de la 115 a la 98

--- app/src/main/java/com/example/smartcoach/ui/_115_modificar_rutina_ejercicios.java
+++ app/src/main/java/com/example/smartcoach/ui/_115_modificar_rutina_ejercicios.java
@@ -1,6 +1,8 @@
 package com.example.smartcoach.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
@@ -27,5 +29,14 @@ public class _115_modificar_rutina_ejercicios extends AppCompatActivity {
         horas = findViewById(R.id.horas);
         scrollView3 = findViewById(R.id.scrollView3);
         btnGuardarCambios_115 = findViewById(R.id.btnGuardarCambios_115);
+
+        btnGuardarCambios_115.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(_115_modificar_rutina_ejercicios.this, _98_ver_rutina_ejercicio_usuario.class);
+                startActivity(intent);
+            }
+        });
     }
 }
