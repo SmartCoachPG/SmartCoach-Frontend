b256584fc5274dcc4e2b03e414bcc337ec5c41db
Johanna Bolívar <bolivarjo@javeriana.edu.co>
2023-10-20 12:47:44 -0500

    127 arreglada

--- app/src/main/java/com/example/smartcoach/ui/_127_ver_gimnasios_para_suscribirse.java
+++ app/src/main/java/com/example/smartcoach/ui/_127_ver_gimnasios_para_suscribirse.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
         import android.widget.ImageButton;
         import android.widget.ScrollView;
         import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartcoach.R;
 
@@ -14,7 +15,7 @@ public class _127_ver_gimnasios_para_suscribirse extends BaseActivityCliente {
     TextView tituloSuscríbete_127;
     EditText editTextGym_127;
     ImageButton btnBusquedaGym_127;
-    ScrollView scrollViewGymSuscribirse_127;
+    RecyclerView recyclerViewGymSuscribirse_127;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +26,7 @@ public class _127_ver_gimnasios_para_suscribirse extends BaseActivityCliente {
         tituloSuscríbete_127 = findViewById(R.id.tituloSuscríbete_127);
         editTextGym_127 = findViewById(R.id.editTextGym_127);
         btnBusquedaGym_127 = findViewById(R.id.btnBusquedaGym_127);
-        scrollViewGymSuscribirse_127 = findViewById(R.id.scrollViewGymSuscribirse);
+        recyclerViewGymSuscribirse_127 = findViewById(R.id.recyclerViewGymSuscribirse_127);
 
     }
 }
